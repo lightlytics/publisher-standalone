7a22638013ec874ed3d0e84729d83e860d1cf528
Tomer <Lilush>
2022-03-07 16:49:17 +0200

    feat: moved core logic to lightlytics-publisher-core

--- index.js
+++ index.js
@@ -1,103 +1,36 @@
-const got = require('got');
-const fs = require('fs');
-const path = require('path');
-const minimist = require('minimist')
-
-function removeAwsCredentials(plan) {
-  if (plan && plan.configuration && plan.configuration.provider_config && plan.configuration.provider_config.aws && plan.configuration.provider_config.aws.expressions) {
-    delete plan['configuration']['provider_config']['aws']['expressions']['access_key']
-    delete plan['configuration']['provider_config']['aws']['expressions']['secret_key']
-  }
-}
-
-const args = minimist(process.argv.slice(2))
-
-if (args['help'] || args['h']) {
-  return console.log("Expected usage: node index.js --dir=\"./working-dir\" --hostname=\"org.lightlytics.com\" --plan=\"./working-dir/plan.json\" --graph=\"./working-dir/graph.dot\" --token=\"collection-token\"")
-}
-
-if (Object.keys(args).length - 1 < 5) {
-  throw new ArgsException("Expected at least 4 cli arguments. usage: node index.js --dir=\"./working-dir\" --hostname=\"org.lightlytics.com\" --plan=\"./working-dir/plan.json\" --graph=\"./working-dir/graph.dot\" --token=\"collection-token\"")
-}
-
-const requiredArgs = ['dir', 'hostname', 'plan', 'graph', 'token']
-
-requiredArgs.forEach(requiredArg => {
-  if(!args[requiredArg])
-    throw new ArgsException(`Missing arg - \"${requiredArg}\" usage: node index.js --${requiredArg}=value. try --help if needed`)
-})
-
-const workingDirArg = args['dir']
-const hostname = args['hostname']
-const terraformPlanPath = args['plan']
-const terraformGraphPath = args['graph']
-const collectionToken = args['token']
+import minimist from 'minimist'
+import {publish} from 'lightlytics-publisher-core'
 
 try {
-  const workingDir = workingDirArg.replace(/\/$/, '')
+  const args = minimist(process.argv.slice(2))
 
-  const modulesPath = path.normalize(`${workingDir}/.terraform/modules/modules.json`);
-  let modules = {}
-  if (fs.existsSync(modulesPath)) {
-    modules = JSON.parse(fs.readFileSync(modulesPath, "utf8"));
-  }
-  const locals = {};
-
-  if (!modules.Modules)
-    modules["Modules"] = [];
-
-  modules["Modules"].push({
-    Key: "root_module",
-    Source: "root_module",
-    Dir: "./"
-  });
-
-  modules.Modules.filter((module) => module.Key && module.Dir && module.Source)
-    .filter(module => fs.existsSync(path.normalize(`${workingDir}/${module.Dir}`)))
-    .forEach((module) => {
-      fs.readdirSync(path.normalize(`${workingDir}/${module.Dir}`)).forEach((fileName) => {
-        const fileExtension = path.parse(fileName).ext;
-        if (fileExtension !== ".tf") return;
-
-        const filePath = `${workingDir}/${module.Dir}/${fileName}`;
-        const moduleContent = fs.readFileSync(filePath, "utf8");
-
-        getLocalsFromModule(moduleContent, locals, module.Source);
-      });
-    });
-
-  const plan = JSON.parse(fs.readFileSync(terraformPlanPath, 'utf8'))
-
-  let graph
-  if (terraformGraphPath) {
-    graph = fs.readFileSync(terraformGraphPath, 'utf8')
+  if (args['help'] || args['h']) {
+    throw "Expected usage: node index.js --dir=\"./working-dir\" --hostname=\"org.lightlytics.com\" --plan=\"./working-dir/plan.json\" --graph=\"./working-dir/graph.dot\" --token=\"collection-token\""
   }
 
-  removeAwsCredentials(plan)
-
-  const publishUrl = `https://${hostname}/api/v1/collection/terraform`
-  const headers = {
-    'X-Lightlytics-Token': collectionToken
+  if (Object.keys(args).length - 1 < 5) {
+    throw "Expected at least 4 cli arguments. usage: node index.js --dir=\"./working-dir\" --hostname=\"org.lightlytics.com\" --plan=\"./working-dir/plan.json\" --graph=\"./working-dir/graph.dot\" --token=\"collection-token\""
   }
 
-  const data = {
-    locals,
-    plan,
-    graph,
-    metadata: {},
-  }
+  const requiredArgs = ['dir', 'hostname', 'plan', 'graph', 'token']
 
-  got.post(publishUrl, {
-    json: data,
-    responseType: 'json',
-    headers
-  }).then((res) => {
-    const eventId = res.body.eventId
-    const customerId = res.body.customerId
+  requiredArgs.forEach(requiredArg => {
+    if (!args[requiredArg]) {
+      throw `Missing arg - \"${requiredArg}\" usage: node index.js --${requiredArg}=value. try --help if needed`
+    }
+  })
 
-    logFormattedSimulation(`https://${hostname}/w/${customerId}/simulations/${eventId}`)
+  const tfWorkingDir = args['dir']
+  const apiUrl = args['hostname']
+  const tfPlan = args['plan']
+  const tfGraph = args['graph']
+  const collectionToken = args['token']
 
-  }).catch(error => console.error(error));
+
+  publish({apiUrl, tfWorkingDir, tfPlan, tfGraph, collectionToken})
+    .then(({eventId, customerId}) => {
+      logFormattedSimulation(`https://${hostname}/w/${customerId}/simulations/${eventId}`)
+    }).catch(error => console.error(error));
 } catch (error) {
   console.error(error)
 }
@@ -109,40 +42,4 @@ ${link}
 > _This comment was added automatically by a git workflow to help DevOps teams predict what will be the impact of the proposed change after completing this PR_`
 
   console.log(pullRequestMessage)
-}
-
-function getLocalsFromModule(module, locals, moduleName) {
-  let blockCnt = 0;
-  let currentBlockLines = "";
-
-  module.split("\n").forEach((line) => {
-    const sanitizedLine = String(line).trim();
-    if (sanitizedLine.startsWith("#")) return;
-
-    if (blockCnt > 0) {
-      currentBlockLines += line;
-    }
-
-    if (blockCnt > 0 && sanitizedLine === "{") {
-      blockCnt++;
-    }
-
-    if (sanitizedLine === "locals {") {
-      currentBlockLines = "";
-      blockCnt = 1;
-    }
-
-    if (sanitizedLine === "}" && blockCnt > 0) {
-      blockCnt--;
-      if (blockCnt === 0) {
-        if (!locals[moduleName]) locals[moduleName] = []
-        locals[moduleName].push(currentBlockLines);
-      }
-    }
-  });
-}
-
-function ArgsException(message) {
-  this.message = message;
-  this.name = 'ArgsException';
 }
